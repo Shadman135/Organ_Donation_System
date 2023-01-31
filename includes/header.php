71d88c74c0c9c3a717310bc378e8968c463f192b
Shadman135 <122206374+Shadman135@users.noreply.github.com>
2023-01-31 14:30:19 +0800

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,68 @@
+    <nav class="navbar fixed-top navbar-toggleable-md navbar-inverse bg-inverse">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarExample" aria-controls="navbarExample" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="container">
+<<<<<<< HEAD
+            <a class="navbar-brand" href="index.php">Blood & Organ Donation Management System</a>
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=aboutus">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=donor">Why Become Donor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="become-donar.php">Become a Donar</a>
+                    </li>
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="search-donor.php">Search Blood</a>
+                    </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">Contact us</a>
+                    </li>
+                 
+=======
+            
+            <div class="collapse navbar-collapse" id="navbarExample">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                        <a class="nav-link" href="index.php">HOME</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=aboutus">ABOUT US</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="page.php?type=donor">BLOG</a>
+                    </li>
+                   
+                 
+                     <li class="nav-item">
+                        <a class="nav-link" href="search-donor.php">SEARCH BLOOD</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="search-organdonor.php">SEARCH ORGAN</a>
+                    </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="contact.php">CONTACT</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="./donor/loginreg/index.php">LOGIN</a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="./donor/loginreg/register.php">REGISTER</a>
+                    </li>
+
+                    
+                    
+                   
+>>>>>>> sprint3
+                 
+                </ul>
+            </div>
+        </div>
+    </nav>
